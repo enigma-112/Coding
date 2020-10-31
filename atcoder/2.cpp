158930f2e17f7204f54eabf2c0b5ec5b3686ee58
Kundan Kumar <rrd51510@gmail.com>
2020-10-31 08:15:18 +0530

    re-upload

---
+++ atcoder/2.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int 
+#define pb push_back
+#define mk make_pair
+#define pii pair<ll,ll> 
+#define vi vector<ll> 
+#define endl '\n'
+void fastio(){
+ 
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+
+ //    #ifndef ONLINE_JUDGE	
+	// freopen("input.txt","r",stdin);	
+	// freopen("output.txt","w",stdout);
+ //    #endif
+}
+
+int main(){
+	
+	fastio();
+
+} 
